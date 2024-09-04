b979959b8ce57fb902ba3207efcf59fbbd87de2b
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-03 21:08:50 -0700

    Update the form object with the contents of posts inside the request object

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,5 +5,7 @@ from myapp.forms import LogForm
 
 def form_view(request):
     form = LogForm()
+    if request.method == 'POST':
+        form = LogForm(request.POST)
     context = {"form" : form}
     return render(request, "home.html", context)
