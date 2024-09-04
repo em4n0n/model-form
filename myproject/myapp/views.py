465842fbe0fe7e929d0743071b393ce212029d4f
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-03 21:06:44 -0700

    Pass form into the context dictionary

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from myapp.forms import LogForm
 
 # Create your views here.
+
+def form_view(request):
+    form = LogForm()
+    context = {"form" : form}
+    return render(request, "home.html", context)
