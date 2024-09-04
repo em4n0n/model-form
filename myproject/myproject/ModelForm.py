9e20cc7d42c89633b1caf1e272616c97a35df8c4
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-03 20:44:09 -0700

    Create a Meta class within the ReservationForm class

--- myproject/myproject/ModelForm.py
+++ myproject/myproject/ModelForm.py
@@ -2,4 +2,6 @@ from django.forms import ModelForm
 from myapp.models import Reservation
 
 class ReservationForm(ModelForm):
-    
+    class Meta:
+        model = Reservation
+        fields = ['name', 'seats', 'content', 'reporter']
