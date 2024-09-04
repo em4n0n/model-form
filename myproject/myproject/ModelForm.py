398a4f5c6d72edd37f5fd42e235de9641c9ee2ab
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-03 20:58:07 -0700

    Create models

--- myproject/myproject/ModelForm.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from myapp.models import Reservation
-
-class ReservationForm(ModelForm):
-    class Meta:
-        model = Reservation
-        fields = ['name', 'seats', 'content', 'reporter']
-        
-form = ReservationForm()
